5729145c1feffe2f7e76e03dd4fb0b3a4dc9a3bc
nblancoz <nblancozappala@gmail.com>
2023-12-29 20:03:05 +0100

    table finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [{ path: '', component: ProductsComponent }];
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { MatTableModule } from '@angular/material/table';
+import { ProductInterface } from '../../interfaces/product-interface';
+import { ProductService } from '../../services/product.service';
 
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [],
+  selector: 'table-basic-example',
+  styleUrl: './products.component.scss',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  standalone: true,
+  imports: [CommonModule],
 })
-export class ProductsComponent {
 
+export class ProductsComponent {
+  public products: Array<ProductInterface> = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
